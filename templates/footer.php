8efb5dd0786233318338b6233bbf4e68bbfca4c4
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2012-08-11 19:56:01 -0400

    removed questionable JS stuff

--- templates/footer.php
+++ templates/footer.php
@@ -17,8 +17,10 @@
 
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<?php /*
 		<script src="js/application.js"></script>
 		<script src="js/bootswatch.js"></script>
+		*/ ?>
 
 		<script type="text/javascript">
 
